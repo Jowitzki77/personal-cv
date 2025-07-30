544060e9b9a140ad90c0dec42d085ba8c754f57e
Jowitzki77 <jakob.jovan.hrustl@gmail.com>
2025-07-30 10:26:00 +0200

    header and logo added

---
+++ src/js/Components/Header/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import logo from "../../../../assets/logo.svg";
+const Logo = () => {
+    return (
+        <div className="header__logo">
+            <img src={logo} alt="Logo" />
+        </div>
+    )
+}
+
+export default Logo;
--- src/js/Pages/Home.js
+++ src/js/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "../Components/Header/Header";
 
 
 const Home = () => {
     return (
         <>
+            <Header type="header"/>
             <p>Home page</p>
+            <Header type="footer"/>
         </>
     )
 }
